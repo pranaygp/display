500845db15c7723add0b26faff1dd3403fca40c6
Benjamin Congdon <bencongdon96@gmail.com>
2017-01-09 14:23:24 -0800

    More modular layout system

--- src/main.js
+++ src/main.js
@@ -1,42 +1,34 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
 var Header = require('./header');
-var AdsPanel = require('./panels/ads');
-var BeatsPanel = require('./panels/beats');
-var MTDPanel = require('./panels/mtd');
-var MeetingTimesPanel = require('./panels/meeting-times');
-var SponsorsPanel = require('./panels/sponsors');
-var EventsPanel = require('./panels/events');
+
 
 var ReactGridLayout = require('react-grid-layout');
 var WidthProvider = require('react-grid-layout').WidthProvider;
 ReactGridLayout = WidthProvider(ReactGridLayout);
-var layout = require('./layout');
-
-
+var dashboard = require('./dashboard');
 
 /**
  * Top-level dashboard component.
  */
-var Dashboard = React.createClass({
+var ACMDisplay = React.createClass({
     render: function() {
+        var widget_divs = dashboard.widgets.map(function(widget) {
+            console.log(widget)
+            return <div key={widget.name}>{React.createElement(widget.component, null)}</div>
+        });
+        console.log(widget_divs)
         return <div>
             <Header />
-            <ReactGridLayout className="layout" layout={layout} cols={24}
+            <ReactGridLayout className="layout" layout={dashboard.layout} cols={24}
                     rowHeight={30} width={1920} autoSize={false} margin={[15, 15]}>
-                <div key={'ads'}><AdsPanel /></div>
-                <div key={'sigs'}><MeetingTimesPanel /></div>
-                <div key={'beats'}><BeatsPanel /></div>
-                <div key={'events'}><EventsPanel /></div>
-                <div key={'mtd'}><MTDPanel /></div>
-                <div key={'sponsors'}><SponsorsPanel /></div>
+                {widget_divs}
             </ReactGridLayout>
         </div>;
     }
 });
 
 ReactDOM.render(
-    React.createElement(Dashboard),
+    React.createElement(ACMDisplay),
     document.getElementById('main')
 );
